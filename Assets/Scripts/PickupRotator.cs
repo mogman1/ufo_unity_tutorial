18040204078e31981f5db7ebdbc1a6c41a0ca563
Shaun Carlson <shaun.carlson@gmail.com>
2016-06-19 00:36:44 -0600

    rotate pickups and add a few to board

---
+++ Assets/Scripts/PickupRotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupRotator : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+	}
+}
