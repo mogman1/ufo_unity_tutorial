5ba04d9418e84317057eccfef96c4ee07a962884
Shaun Carlson <shaun.carlson@gmail.com>
2016-06-19 01:00:52 -0600

    add ability to collect pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public int object_count;
 
 	private Rigidbody2D rb2d;
 
 	void Start() {
+		object_count = 0;
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
@@ -15,5 +17,12 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis("Vertical");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb2d.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			object_count++;
+			other.gameObject.SetActive (false);
+		}
 	}
 }
